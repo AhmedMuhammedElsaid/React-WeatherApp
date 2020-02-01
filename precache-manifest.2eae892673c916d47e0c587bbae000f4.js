646617ebcce8f25abf25e167466d9ec2067d87bc
AhmedMuhammedElsaid <ahmed.muhammed.elsaid@gmail.com>
2020-02-01 18:03:08 +0200

    Updates

---
+++ precache-manifest.2eae892673c916d47e0c587bbae000f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991f8e7f2e7575d1ae316136d3779118",
+    "url": "/React-WeatherApp/index.html"
+  },
+  {
+    "revision": "3a2a05cd80a7eb630629",
+    "url": "/React-WeatherApp/static/css/main.329f0f35.chunk.css"
+  },
+  {
+    "revision": "f388e2cc8256990de3d4",
+    "url": "/React-WeatherApp/static/js/2.2895cb3d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-WeatherApp/static/js/2.2895cb3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a2a05cd80a7eb630629",
+    "url": "/React-WeatherApp/static/js/main.0f32b735.chunk.js"
+  },
+  {
+    "revision": "46644d709b0df77f72a7",
+    "url": "/React-WeatherApp/static/js/runtime-main.035ff59e.js"
+  }
+]);
